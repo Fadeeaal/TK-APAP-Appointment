3ffa3386b57a4abc0a6fb99c4af72e4d1a6dbd40
Fadeeaal <rakha.fadil@ui.ac.id>
2024-12-05 09:44:17 +0700

    Update main

--- src/main/java/apap/tk/appointment/config/CorsConfig.java
+++ src/main/java/apap/tk/appointment/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package apap.tk.appointment.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,18 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8086")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                        .allowedOrigins("http://localhost:5173","http://localhost:8086", "https://apap-2207.cs.ui.ac.id", "https://apap-2206.cs.ui.ac.id", "https://apap-2201.cs.ui.ac.id", "https://apap-2204.cs.ui.ac.id", "https://apap-2203.cs.ui.ac.id", "https://apap-2205.cs.ui.ac.id")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
 }
-
--- src/main/java/apap/tk/appointment/service/PatientService.java
+++
@@ -1,8 +0,0 @@
-package apap.tk.appointment.service;
-
-import apap.tk.appointment.model.Patient;
-
-public interface PatientService {
-    Patient addPatient(Patient patient);
-    Patient getPatientByNIK(String nik);
-}
